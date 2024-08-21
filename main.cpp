8e642d4efb589bf04b83b3ef802df6760a3a6090
ANorman <alex1.norman2@gmail.com>
2024-08-21 13:46:33 -0500

    Extracted functionality class

--- main.cpp
+++ main.cpp
@@ -57,6 +57,7 @@
 #include "./headers/aes.h"
 #include "./headers/huffman.h"
 #include "./headers/markov.h"
+#include "./headers/crypto.h"
 #include <iostream>
 #include <array>
 #include <set>
@@ -82,25 +83,20 @@ const array<int, variantBitLength> variantBits = {1,2,4,8,16,32,64,96,112,120,12
 
 mutex m, m1, m2;
 
-void encodeText(array<unsigned char, 16>&, const array<unsigned char, 16>&);//plainText XOR cipherText 
-void genRandomSegmented(set<array<unsigned char, 16>>&, int, int);//set, size, numsegments.
-void genRandomSegmented2(set<array<unsigned char, 16>>&, int, int, set<array<unsigned char, 16>>&);//set, size, numsegments, another set. to prevent duplicates between threads
-void toCharArray(array<unsigned char, 16>&, const __uint128_t &);//128 bit number to char array
-void swapBits(array<unsigned char, 16>&, const int &);//randomly swap n numbers of bits
-void markovDifference(const array<array<double, 2>, 2>&, const array<array<double, 2>, 2>&, double &);//measure difference between 2 transition matrices
-
 void threadMain(array<double, variantBitLength> &differenceTotal, int &countTotal, const set<array<unsigned char, 16>>&setPlainTexts, set<array<unsigned char, 16>>&setTotalKeys)//fix later.
 {
     //set<array<unsigned char, 16>> setPlainTexts;
     set<array<unsigned char, 16>> setKeys;
     //genRandomSegmented(setPlainTexts, NUM_PLAINTEXTS / NUM_THREADS, 10);
     //genRandomSegmented(setKeys, NUM_KEYS / NUM_THREADS, 10);
-    genRandomSegmented2(setKeys, NUM_KEYS / NUM_THREADS, NUM_SEGMENTS, setTotalKeys);
+    Crypto::genRandomSegmented(setKeys, NUM_KEYS / NUM_THREADS, NUM_SEGMENTS, setTotalKeys, m2);
 
     AES aes;
     array<unsigned char, 16> state;
     array<array<double, 2>, 2> tMatrixControl;
     array<array<double, 2>, 2> tMatrixVariant;
+    array<double, variantBitLength> differenceTotalTemp;
+    int countTotalLocal = 0;
 
     for (const auto &plainText : setPlainTexts)
     {
@@ -108,7 +104,7 @@ void threadMain(array<double, variantBitLength> &differenceTotal, int &countTota
         {
             state = plainText;
             aes.encrypt(state, key);//ciphertext now
-            encodeText(state, plainText);
+            Crypto::encodeText(state, plainText);
             Huffman huffman(state);
             Markov::generateMarkovTransitionMatrix(huffman.getHuffmanCodes(), tMatrixControl);
             
@@ -120,22 +116,28 @@ void threadMain(array<double, variantBitLength> &differenceTotal, int &countTota
                 stateVariant=plainText;
                 keyVariant = key;
 
-                swapBits(keyVariant, variantBits[i]);
+                Crypto::swapBits(keyVariant, variantBits[i]);
                 aes.encrypt(stateVariant, keyVariant);//ciphertext now
-                encodeText(stateVariant, plainText);
+                Crypto::encodeText(stateVariant, plainText);
                 Huffman huffmanVariant(stateVariant);
 
                 Markov::generateMarkovTransitionMatrix(huffmanVariant.getHuffmanCodes(), tMatrixVariant);
                 double difference;
-                markovDifference(tMatrixControl, tMatrixVariant, difference);
-                m.lock();
-                differenceTotal[i] += difference;
-                m.unlock();
+                Crypto::markovDifference(tMatrixControl, tMatrixVariant, difference);
+                differenceTotalTemp[i] += difference;
             }
-            m1.lock(); 
-            countTotal++;
-            m1.unlock();
+            countTotalLocal++;
         }
+    }
+    
+    m1.lock();
+    countTotal+=countTotalLocal;
+    m1.unlock();
+    for(int i=0;i<variantBitLength;i++)
+    {
+        m.lock();
+        differenceTotal[i]+=differenceTotalTemp[i];
+        m.unlock();
     }
 }
 
@@ -145,7 +147,7 @@ int main()
     auto start = chrono::high_resolution_clock::now();
 
     set<array<unsigned char, 16>> setPlainTexts;
-    genRandomSegmented(setPlainTexts, NUM_PLAINTEXTS, NUM_SEGMENTS);//generate plaintexts
+    Crypto::genRandomSegmented(setPlainTexts, NUM_PLAINTEXTS, NUM_SEGMENTS);//generate plaintexts
     set<array<unsigned char, 16>> setTotalKeys;//create set to prevent duplication of keys between threads
     mutex m;
     array<double, variantBitLength> differenceTotal{};//for calculating avg difference of matrices
@@ -229,123 +231,4 @@ int main()
     */
 
     return 0;
-}
-
-void encodeText(array<unsigned char, 16> &state, const array<unsigned char, 16> &plainText)
-{
-    for(int i=0;i<16;i++) 
-        state[i] ^= plainText[i];
-}
-
-void markovDifference(const array<array<double, 2>, 2>&mControl, const array<array<double, 2>, 2>&mVariant, double &difference)
-{
-    double total = 0;
-    for(int i=0;i<2;i++)
-    {
-        for(int j=0;j<2;j++)
-        {
-            double d = mControl[i][j] - mVariant[i][j];
-            total += (d*d);
-        }
-    }
-    difference = sqrt(total);
-}
-
-void swapBits(array<unsigned char, 16>&key, const int &num)
-{
-    if(num != 128)//1 to 64 bits flipped
-    {
-        set<int> indices;//makes sure no duplicates of flipping
-        for(int i=0;i<num;i++)
-        {
-            int index = rand() % 128;
-            while(indices.count(index))
-                index = rand() % 128;
-            indices.insert(index);
-            
-
-            int byteIndex = index / 8;
-            int bitIndex = index % 8;
-
-            unsigned char c = 1 << bitIndex;//creates a byte with a 1 at the index
-            key[byteIndex] ^= c;//XOR only affects targeted index
-        }
-    }
-    else//flip all 128 bits
-    {
-        for(int i=0;i<128;i++)
-        {
-            int byteIndex = i / 8;
-            int bitIndex = i % 8;
-            unsigned char c = 1 << bitIndex;
-            key[byteIndex] ^= c;
-        }
-    }
-}
-
-void genRandomSegmented(set<array<unsigned char, 16>> &charSet, int size, int segments)
-{
-    random_device seed;//seed for random number generator
-    mt19937_64 gen(seed());//random number generator
-    std::uniform_int_distribution<uint64_t> dis64(0, ~uint64_t(0));//setting up uniform distribution for 64 bit number generation
-
-    
-    __uint128_t segmentSize = UINT128_MAX / segments;
-    array<unsigned char, 16> temp;
-    __int128_t num;
-    int index = 0;//keeps track of the segment we are working in. Higher end segments may have 1 less instance than lower end segments
-    while(charSet.size() < size)
-    {
-        num = (dis64(gen)) << 64 | dis64(gen);//generates 2 64 bit numbers and combines
-        num = (num % segmentSize) + (index * segmentSize);//ensures it falls in the range
-    
-        toCharArray(temp, num);//convert 128 bit number to the array
-        bool added = charSet.insert(temp).second;//prevents advancing to the next segment in case of duplicate
-        if(added)
-        {
-            index++;
-            if(index == segments)
-                index = 0;
-        }
-    }
-}
-
-void genRandomSegmented2(set<array<unsigned char, 16>> &charSet, int size, int segments, set<array<unsigned char, 16>> &totalSet)//for use with keys only in threads
-{
-    random_device seed;//seed for random number generator
-    mt19937_64 gen(seed());//random number generator
-    std::uniform_int_distribution<uint64_t> dis64(0, ~uint64_t(0));//setting up uniform distribution for 64 bit number generation
-
-    
-    __uint128_t segmentSize = UINT128_MAX / segments;
-    array<unsigned char, 16> temp;
-    __int128_t num;
-    int index = 0;//keeps track of the segment we are working in. Higher end segments may have 1 less instance than lower end segments
-    while(charSet.size() < size)
-    {
-        num = (dis64(gen)) << 64 | dis64(gen);//generates 2 64 bit numbers and combines
-        num = (num % segmentSize) + (index * segmentSize);//ensures it falls in the range
-    
-        toCharArray(temp, num);//convert 128 bit number to the array
-        m2.lock();
-        bool added = totalSet.insert(temp).second;//prevents advancing to the next segment in case of duplicate in total set
-        m2.unlock();
-        if(added)
-        {
-            charSet.insert(temp);//add to local set
-            index++;
-            if(index == segments)
-                index = 0;
-        }
-    }
-}
-
-void toCharArray(array<unsigned char, 16>&arr, const __uint128_t &num)
-{
-    __uint128_t temp = num;
-    for(int i=0;i<16;i++)
-    {
-        arr[15 - i] = temp & 0xFF;//extract lowest 8 bits
-        temp >>= 8;//bitwise shift right 8 bits
-    }
 }
---
+++ src/crypto.cpp
@@ -0,0 +1,122 @@
+#include "../headers/crypto.h"
+#include <cmath>
+#include <random>
+
+void Crypto::encodeText(std::array<unsigned char, 16> &state, const std::array<unsigned char, 16> &plainText)
+{
+    for(int i=0;i<16;i++) 
+        state[i] ^= plainText[i];
+}
+
+void Crypto::markovDifference(const std::array<std::array<double, 2>, 2>&mControl, const std::array<std::array<double, 2>, 2>&mVariant, double &difference)
+{
+    double total = 0;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            double d = mControl[i][j] - mVariant[i][j];
+            total += (d*d);
+        }
+    }
+    difference = std::sqrt(total);
+}
+
+void Crypto::swapBits(std::array<unsigned char, 16>&key, const int &num)
+{
+    if(num != 128)//1 to 64 bits flipped
+    {
+        std::set<int> indices;//makes sure no duplicates of flipping
+        for(int i=0;i<num;i++)
+        {
+            int index = rand() % 128;
+            while(indices.count(index))
+                index = rand() % 128;
+            indices.insert(index);
+            
+
+            int byteIndex = index / 8;
+            int bitIndex = index % 8;
+
+            unsigned char c = 1 << bitIndex;//creates a byte with a 1 at the index
+            key[byteIndex] ^= c;//XOR only affects targeted index
+        }
+    }
+    else//flip all 128 bits
+    {
+        for(int i=0;i<128;i++)
+        {
+            int byteIndex = i / 8;
+            int bitIndex = i % 8;
+            unsigned char c = 1 << bitIndex;
+            key[byteIndex] ^= c;
+        }
+    }
+}
+
+void Crypto::genRandomSegmented(std::set<std::array<unsigned char, 16>> &charSet, int size, int segments)
+{
+    std::random_device seed;//seed for random number generator
+    std::mt19937_64 gen(seed());//random number generator
+    std::uniform_int_distribution<uint64_t> dis64(0, ~uint64_t(0));//setting up uniform distribution for 64 bit number generation
+
+    
+    __uint128_t segmentSize = UINT128_MAX / segments;
+    std::array<unsigned char, 16> temp;
+    __int128_t num;
+    int index = 0;//keeps track of the segment we are working in. Higher end segments may have 1 less instance than lower end segments
+    while(charSet.size() < size)
+    {
+        num = (dis64(gen)) << 64 | dis64(gen);//generates 2 64 bit numbers and combines
+        num = (num % segmentSize) + (index * segmentSize);//ensures it falls in the range
+    
+        toCharArray(temp, num);//convert 128 bit number to the array
+        bool added = charSet.insert(temp).second;//prevents advancing to the next segment in case of duplicate
+        if(added)
+        {
+            index++;
+            if(index == segments)
+                index = 0;
+        }
+    }
+}
+
+void Crypto::genRandomSegmented(std::set<std::array<unsigned char, 16>> &charSet, int size, int segments, std::set<std::array<unsigned char, 16>> &totalSet, std::mutex &m2)//for use with keys only in threads
+{
+    std::random_device seed;//seed for random number generator
+    std::mt19937_64 gen(seed());//random number generator
+    std::uniform_int_distribution<uint64_t> dis64(0, ~uint64_t(0));//setting up uniform distribution for 64 bit number generation
+
+    
+    __uint128_t segmentSize = UINT128_MAX / segments;
+    std::array<unsigned char, 16> temp;
+    __int128_t num;
+    int index = 0;//keeps track of the segment we are working in. Higher end segments may have 1 less instance than lower end segments
+    while(charSet.size() < size)
+    {
+        num = (dis64(gen)) << 64 | dis64(gen);//generates 2 64 bit numbers and combines
+        num = (num % segmentSize) + (index * segmentSize);//ensures it falls in the range
+    
+        toCharArray(temp, num);//convert 128 bit number to the array
+        m2.lock();
+        bool added = totalSet.insert(temp).second;//prevents advancing to the next segment in case of duplicate in total set
+        m2.unlock();
+        if(added)
+        {
+            charSet.insert(temp);//add to local set
+            index++;
+            if(index == segments)
+                index = 0;
+        }
+    }
+}
+
+void Crypto::toCharArray(std::array<unsigned char, 16>&arr, const __uint128_t &num)
+{
+    __uint128_t temp = num;
+    for(int i=0;i<16;i++)
+    {
+        arr[15 - i] = temp & 0xFF;//extract lowest 8 bits
+        temp >>= 8;//bitwise shift right 8 bits
+    }
+}
